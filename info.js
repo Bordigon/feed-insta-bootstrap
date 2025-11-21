de84f96819d587fc9c880cb43ad5f6c6f3a77fb6
Bordigon <gistavovg@gmail.com>
2025-11-21 09:25:30 -0500

    finalizado

---
+++ info.js
@@ -0,0 +1,49 @@
+export const info = {"photos":[
+		{
+			"title":"f4ni_o19",
+			"image_url":"assets/photo_7.jpg",
+			"description":"",
+			"date":"26/11/2025"
+		},{
+			"title":"Una aventurita por Santander y Santillana del Mar",
+			"image_url":"https://scontent.cdninstagram.com/v/t51.82787-15/527343264_18197958565309797_566107223610699714_n.webp?_nc_cat=107&ig_cache_key=MzY4OTE4ODc3MTY1MjY0NDk0MQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTQ0MC5zZHIuQzMifQ%3D%3D&_nc_ohc=vNyUgTG5ZZUQ7kNvwGiWIii&_nc_oc=AdkAS5a5iLviCgrYsq03ffAXJ_rakDfLyFHAfASrA8i04LPXUBGHLA80x56E_SMlZok&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=VsBTwHxYJmsuK5imoxJFrQ&oh=00_Afg-khfPtcQA-i2JTpIciNMEHtiRCu5pyPIPvNZyipUbEg&oe=69255518",
+			"description":"Siempre es bonito crear recuerdos, especialmente con la gente que amas ❤️",
+			"date":"31/7/2025"
+		},{
+			"title":"En la Warner el verano del año pasado!!!",
+			"image_url":"assets/photo_2.jpeg",
+			"description":"Pd: hay muchas fotos que no he subido y voy a subir",
+			"date":"28/2/2022"
+		},{
+			"title":"Mar azul",
+			"image_url":"assets/photo_3.jpg",
+			"description":"",
+			"date":""
+		},{
+			"title":"",
+			"image_url":"assets/photo_4.jpeg",
+			"description":"💗",
+			"date":"18/8/2024"
+		},{
+			"title":"Córdoba mayo 2025 🪻🌸💃🏻",
+			"image_url":"assets/photo_5.jpeg",
+			"description":"",
+			"date":"2/6/2025"
+		},{
+			"title":"Cumple",
+			"image_url":"assets/photo_6.jpeg",
+			"description":"Muchas gracias a todos por venir!!!!",
+			"date":"4/10/2023"
+		},{
+			"title":"",
+			"image_url":"assets/photo_8.jpg",
+			"description":"",
+			"date":""
+		},		{
+			"title":"patrii.allegraa",
+			"image_url":"https://scontent.cdninstagram.com/v/t51.82787-15/553990817_18398240056187147_6945641845925776684_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&_nc_cat=102&ig_cache_key=MzczMDk3MDU5MzkwMzYxNjM5OA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEyODR4MTcxMi5zZHIuQzMifQ%3D%3D&_nc_ohc=kBRhbuAlZPEQ7kNvwH-idR0&_nc_oc=AdnJ2eHfTN3td4N3xhrDTNSl-F5L2USzXB_fb1KDbydAB7Pu1qXI1OTEjhYyN2SSs_E&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=7DcFtjeUHPcJ8jxXzesfng&oh=00_AfhISRQqdmFFk473DB1_O39bU6mylu14rfDg5o6FiKHkSw&oe=69253FC2",
+			"description":"🪶",
+			"date":"27/09/2025"
+		}
+	]
+}
---
+++ utils.js
@@ -0,0 +1,86 @@
+import {info} from "./info.js"
+
+const counter = 9;
+
+const exampleModal = document.getElementById('uploadPhotoModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title').innerHTML()
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `${modalTitle}`
+    modalBodyInput.value = recipient
+  })
+}
+
+const gripOneTemplate = ()=>{
+	var display = [];
+	const photo = info.photos;
+	for(var t = 0; t<counter; t++){
+	const onePhoto = photo[t];
+	const likes = Math.trunc(Math.random()*1000);
+	display = display + `
+		<div class="card my-3">
+			<div class="card-header bg-white d-flex justify-content-between mx-0 px-0 ">
+				<h5 class="card-tittle m-2">${onePhoto.title}</h5>
+				<small class="text-muted mx-2">${onePhoto.date}</small>
+			</div>
+			<img class="photo" src=${onePhoto.image_url}>
+			<div class="card-body">
+				<strong>${likes} Likes</strong>
+				<p class="card-text">${onePhoto.description}</p>
+			</div>
+		</div>
+	`
+	}
+	console.log(display);
+	return display
+}
+
+const gripAllTemplate = () =>{
+	var result = `<div class="container-fluid">`
+	const photo = info.photos;
+	for(var t = 0; t<counter; t++){
+		result = result + `<div class="row mx-1 my-4">`;
+		for(var k = 0; k<counter && k<3; k++){
+			const onePhoto=photo[t];
+			console.log(onePhoto);
+			result = result + `
+				<div class="col-sm-4">
+					<img class="img-fluid h-100 w-100 photoAll"  src=${onePhoto['image_url']}>
+				</div>
+			`;
+			t++;
+		}
+		t--;
+		result += `</div>`
+	}
+	result = result + `</div>`
+	return result;
+
+}
+
+const root = document.getElementById('root')
+root.innerHTML = gripAllTemplate();
+const gripAll = document.getElementById('gripAll')
+const gripOne = document.getElementById('gripOne')
+	gripAll.addEventListener("click", ()=>{
+
+	root.innerHTML = `${gripAllTemplate()}`
+	});
+
+	gripOne.addEventListener("click", ()=>{
+	root.innerHTML = `<div class="d-flex justify-content-center" ><div class="col-sm-4"> ${gripOneTemplate()}</div></div>`
+	});
+
+
+
+
